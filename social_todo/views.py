cfe03682e6818e740f4f42a2c95bdf64e7fc0439
aujerding <aujer.ding@yale.edu>
2016-02-19 00:32:09 +0000

    asd

--- social_todo/views.py
+++ social_todo/views.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
 
+import textwrap
 
+from django.http import HttpResponse
+from django.views.generic.base import View
 from django.shortcuts import render
 from django.http import HttpResponse
 import textwrap
 from six.moves.BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from django.http import HttpResponseRedirect
+from .forms import NameForm
 
 # Create your views here.
 
-def index(self):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+def index(request):
+    response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Greetings to the world</title>
+            </head>
+            <body>
+                <h1>Greetings to the world</h1>
+                <p>Hello, world!</p>
+            </body>
+            </html>
+        ''')
+    return HttpResponse(response_text)
+
+def test(request):
+    return HttpResponse("test passed")
 
     
 def say_whatsup(request):
