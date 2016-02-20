d8137f19089246848fa408bf407ea8095dc94725
aujerding <aujer.ding@yale.edu>
2016-02-20 01:05:41 +0000

    Added both forms, Kyle

--- splash/views.py
+++ splash/views.py
@@ -3,55 +3,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .forms import NameForm
+from .forms import loginForm
 from .forms import RegisterForm
 
 # Create your views here.
-
-
-
 def index(request):
-    get_registration(request)
-    registrationForm = RegistrationForm()
-
-    return render(request, 'index.html')
+    # if request.user.is_authenticated():
+    #     logger.error('Something went right!')
+    #     return render(request, 'splash/index.html', {'user': request.user, 'new_task': newTaskForm, 'tasks': tasks})
+    # else:
+        registerForm = RegisterForm()
+        loginform = loginForm()
+        return render(request, 'index.html', {'login': loginform, 'register': RegisterForm})
     
     
-def get_registration(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RegisterForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            # IMPORTANT- changed it from a different url back to the homepage
-            return HttpResponseRedirect('')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = RegisterForm() 
-        
-    return render(request, 'index.html', {'form': form})
 
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'index.html', {'form': form})
-    
